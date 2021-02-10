457fb828b4164cdd3590ef5f8e14cd21e289fb61
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-10 03:07:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(final_list)
-  final_list.each { |name| badge_maker(final_list)}
+def batch_badge_creator(list)
+  list.each do |name|
+    "Hello, my name is #{name}"
+  end
 end
+
+  
